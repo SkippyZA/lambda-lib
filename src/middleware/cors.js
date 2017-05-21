aaeff25240c2f624a8ba494f73d8f090039b719f
Steven Inskip <inskips@gmail.com>
2017-05-21 23:20:56 +0200

    cors middleware to be conditional

--- src/middleware/cors.js
+++ src/middleware/cors.js
@@ -1,15 +1,17 @@
 /**
  * Add cors headers to event
  */
-export default () => (req, res) => {
-  const corsHeaders = {
-    'Access-Control-Allow-Origin': '*',
-    'Access-Control-Allow-Credentials': true,
-    'Access-Control-Allow-Methods': 'POST, GET, PUT, PATCH, DELETE, OPTIONS'
-  }
+export default (useCors) => (req, res) => {
+  if (useCors) {
+    const corsHeaders = {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Credentials': true,
+      'Access-Control-Allow-Methods': 'POST, GET, PUT, PATCH, DELETE, OPTIONS'
+    }
 
-  res.headers = {
-    ...corsHeaders,
-    ...res.headers
+    res.headers = {
+      ...corsHeaders,
+      ...res.headers
+    }
   }
 }
