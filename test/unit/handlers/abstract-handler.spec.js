10a786eca94265d5166ed400887039ada7194c01
Steven Inskip <inskips@gmail.com>
2017-11-07 14:58:48 +0200

    Add abstract registerPlugin test

--- test/unit/handlers/abstract-handler.spec.js
+++ test/unit/handlers/abstract-handler.spec.js
@@ -1,5 +1,30 @@
+import { expect } from 'chai'
+import sinon from 'sinon'
+import AbstractLambdaPlugin from '../../../src/plugins/abstract-lambda-plugin'
+import AbstractHandler from '../../../src/handlers/abstract-handler'
+
 describe('abstract handler', () => {
   describe('#registerPlugin', () => {
-    it('should register a plugin')
+    class SampleHandler extends AbstractHandler {
+    }
+
+    it('should register a plugin', () => {
+      const pluginMock = sinon.createStubInstance(AbstractLambdaPlugin)
+      pluginMock.isSupportedType.returns(true)
+
+      const handler = new SampleHandler()
+      handler.registerPlugin(pluginMock)
+
+      handler.registeredPlugins.length.should.equal(1)
+    })
+
+    it('should throw an error if not a valid plugin type', () => {
+      const pluginMock = sinon.createStubInstance(AbstractLambdaPlugin)
+      pluginMock.isSupportedType.returns(false)
+
+      const handler = new SampleHandler()
+
+      expect(() => handler.registerPlugin(pluginMock)).to.throw(TypeError)
+    })
   })
 })
