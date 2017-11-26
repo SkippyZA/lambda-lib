11e29adf12cd1161064689d8053cd64821823085
Steven Inskip <inskips@gmail.com>
2017-11-26 18:56:33 +0200

    Added test for overwriting default headers

--- test/unit/extras/axios-interceptor.spec.js
+++ test/unit/extras/axios-interceptor.spec.js
@@ -1,6 +1,10 @@
 import AxiosInterceptor from '../../../src/extras/axios-interceptor'
 
 describe('axios interceptor', () => {
+  beforeEach(() => {
+    global.CONTEXT = undefined
+  })
+
   it('should not adjust the headers if no global context is available', () => {
     const config = {
       headers: {
@@ -32,5 +36,21 @@ describe('axios interceptor', () => {
         'x-correlation-id': 'some-hash-here'
       }
     })
+  })
+
+  it('should favour the user set headers over that of the global context', () => {
+    global.CONTEXT = {
+      'x-correlation-test': 'context'
+    }
+
+    const config = {
+      headers: {
+        'x-correlation-test': 'request'
+      }
+    }
+
+    const patchedConfig = AxiosInterceptor(config)
+
+    patchedConfig.should.deep.equal(config)
   })
 })
