d248c4f5b66adbbb484edd55ab57e517815a1861
Steven Inskip <inskips@gmail.com>
2017-11-27 21:00:46 +0200

    More verbose bunyan stream extra

--- src/extras/bunyan-stream.js
+++ src/extras/bunyan-stream.js
@@ -2,19 +2,29 @@
  * Bunyan stream to merge in global context with each log request.
  */
 function BunyanStream () {
-  this.write = function (logObj) {
-    if (typeof (logObj) === 'object') {
-      const globalContext = global.CONTEXT || {}
-      const logRecord = Object.assign({}, globalContext, logObj)
+  this.write = function (rec) {
+    let logObject
 
-      // Clean up x-rrid and log as rrid
-      if (logRecord['x-rrid']) {
-        logRecord['rrid'] = logRecord['x-rrid']
-        delete logRecord['x-rrid']
+    if (typeof (rec) !== 'object') {
+      try {
+        logObject = JSON.parse(rec)
+      } catch (err) {
+        console.error('error: raw stream got a non-object record: %j', rec)
+        return
       }
-
-      process.stdout.write(JSON.stringify(logRecord) + '\n')
+    } else {
+      logObject = rec
     }
+
+    const globalContext = global.CONTEXT || {}
+    const logRecord = Object.assign({}, globalContext, logObject)
+
+    if (logRecord['x-rrid']) {
+      logRecord['rrid'] = logRecord['x-rrid']
+      delete logRecord['x-rrid']
+    }
+
+    process.stdout.write(JSON.stringify(logRecord) + '\n')
   }
 }
 
