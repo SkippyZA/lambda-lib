49374cf7e6f44ed1368ce0a224127f3f16c07157
Steven Inskip <inskips@gmail.com>
2017-11-29 20:39:57 +0200

    Preparse apigateway event body to json

--- src/plugins/apply-api-gateway-body.js
+++ src/plugins/apply-api-gateway-body.js
@@ -1,6 +1,7 @@
 import AbstractLambdaPlugin from './abstract-lambda-plugin'
 import PluginHook from '../enums/hooks'
 import LambdaType from '../enums/lambda-type'
+import safeJson from '../utils/safe-json'
 
 export default class ApplyApiGatewayBody extends AbstractLambdaPlugin {
   constructor () {
@@ -11,7 +12,7 @@ export default class ApplyApiGatewayBody extends AbstractLambdaPlugin {
 
   applyBody () {
     return (req, res, data) => {
-      res.body = data
+      res.body = safeJson(data)
     }
   }
 }
