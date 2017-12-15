44c05f707d74b858a513d2d9ad120b11eedc964a
Steven Inskip <inskips@gmail.com>
2017-12-15 16:24:08 +0200

    Added new tests for global error status code maps

--- test/unit/plugins/error-status-code-map.spec.js
+++ test/unit/plugins/error-status-code-map.spec.js
@@ -1,6 +1,17 @@
 import { expect } from 'chai'
 import ErrorStatusCodeMap from '../../../src/plugins/error-status-code-map'
 
+const GLOBAL_ERROR_MAP = [
+  {
+    error: EvalError,
+    status: 401
+  },
+  {
+    error: ReferenceError,
+    status: 500
+  }
+]
+
 const SAMPLE_ERROR_MAP = [
   {
     error: TypeError,
@@ -38,7 +49,27 @@ describe('error-status-code map', () => {
   })
 
   describe('when supplied with a global error status code map', () => {
-    it('should make use of the global status codes if no local map supplied')
-    it('should favour the local mapping status code instead of the global map when errors clash')
+    let req, res
+
+    beforeEach(() => {
+      req = {}
+      res = {}
+    })
+
+    it('should make use of the global status codes if no local map supplied', () => {
+      const errorStatusCodeMap = new ErrorStatusCodeMap(GLOBAL_ERROR_MAP)
+      const mapFn = errorStatusCodeMap.errorMapper()
+
+      mapFn(req, res, new EvalError('eval error'))
+      expect(res.statusCode).to.equal(401)
+    })
+
+    it('should favour the local mapping status code instead of the global map when errors clash', () => {
+      const errorStatusCodeMap = new ErrorStatusCodeMap(GLOBAL_ERROR_MAP)
+      const mapFn = errorStatusCodeMap.errorMapper(SAMPLE_ERROR_MAP)
+
+      mapFn(req, res, new ReferenceError('reference error'))
+      expect(res.statusCode).to.equal(404)
+    })
   })
 })
