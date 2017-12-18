228703a7141b8edda6bbc29b28cd25e82610f03c
Steven Inskip <inskips@gmail.com>
2017-12-18 17:54:53 +0200

    Finally adding tests for cors

--- test/unit/plugins/cors.spec.js
+++ test/unit/plugins/cors.spec.js
@@ -1,3 +1,4 @@
+import CorsPlugin from '../../../src/plugins/cors'
 import { should } from 'chai'
 
 describe('cors plugin', () => {
@@ -5,9 +6,40 @@ describe('cors plugin', () => {
     should()
   })
 
-  it('should add headers to an empty header object')
+  it('should add headers to an empty header object', () => {
+    const req = {}
+    const res = { statusCode: 200, body: '', headers: {} }
 
-  it('should append CORS headers to an object with existing headers')
+    const cors = new CorsPlugin()
+    cors.corsPlugin(true)(req, res, null)
 
-  it('should not overwrite existing CORS headers')
+    res.headers['Access-Control-Allow-Origin'].should.equal('*')
+    res.headers['Access-Control-Allow-Credentials'].should.equal(true)
+    res.headers['Access-Control-Allow-Methods'].should.equal('POST, GET, PUT, PATCH, DELETE, OPTIONS')
+  })
+
+  it('should append CORS headers to an object with existing headers', () => {
+    const req = {}
+    const res = { statusCode: 200, body: '', headers: { 'original-header': 'hello world' } }
+
+    const cors = new CorsPlugin()
+    cors.corsPlugin(true)(req, res, null)
+
+    res.headers['original-header'].should.equal('hello world')
+    res.headers['Access-Control-Allow-Origin'].should.equal('*')
+    res.headers['Access-Control-Allow-Credentials'].should.equal(true)
+    res.headers['Access-Control-Allow-Methods'].should.equal('POST, GET, PUT, PATCH, DELETE, OPTIONS')
+  })
+
+  it('should not overwrite existing CORS headers', () => {
+    const req = {}
+    const res = { statusCode: 200, body: '', headers: { 'Access-Control-Allow-Methods': 'GET, OPTIONS' } }
+
+    const cors = new CorsPlugin()
+    cors.corsPlugin(true)(req, res, null)
+
+    res.headers['Access-Control-Allow-Origin'].should.equal('*')
+    res.headers['Access-Control-Allow-Credentials'].should.equal(true)
+    res.headers['Access-Control-Allow-Methods'].should.equal('GET, OPTIONS')
+  })
 })
