83d6cc5262f2148ce24928acf52497a6333ecc6d
Steven Inskip <inskips@gmail.com>
2018-02-05 20:11:56 +0200

    remove console logs

--- src/plugins/api-gateway-response-object-mapper.js
+++ src/plugins/api-gateway-response-object-mapper.js
@@ -13,21 +13,12 @@ export default class ApiGatewayResponseObjectMapper extends AbstractLambdaPlugin
   responseObjectMapper () {
     return (req, res, data, context, done) => {
       if (data && data instanceof ApiGatewayResponse) {
-        console.log('-- data -----------------------------------------------')
-        console.log(data)
-        console.log('-- res -----------------------------------------------')
-        console.log(res)
-
         res.statusCode = data.statusCode || res.statusCode
         res.body = data.body || res.body
         res.headers = {
           ...res.headers,
           ...data.headers
         }
-
-        console.log('-- updated res -----------------------------------------------')
-        console.log(res)
-        console.log('-------------------------------------------------')
       }
 
       done()
