3104d848a407c3524619712bfb6abf4e8e70e29a
Chris Brand <chris.brand@iflix.com>
2018-04-20 12:07:04 +0200

    Expose abstract handler in module index

--- src/index.js
+++ src/index.js
@@ -5,5 +5,6 @@ export { default as Enums } from './enums'
 export { default as ApiGatewayResponse } from './api-gateway-response'
 
 export { default as HandlerController } from './handler-controller'
+export { default as AbstractHandler } from './handlers/abstract-handler'
 export { default as ApiGateway } from './handlers/api-gateway'
 export { default as Lambda } from './handlers/lambda'
