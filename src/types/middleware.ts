958d8728c9cfa97fcb5d86b8b8b97a741486356e
Steven Inskip <inskips@gmail.com>
2019-01-27 04:24:16 +0100

    Make middleware fn done method required

--- src/types/middleware.ts
+++ src/types/middleware.ts
@@ -1,3 +1,3 @@
 import { Context } from 'aws-lambda'
 
-export type MiddlewareFunction = (req: any, res: any, event: any, context: Context, done?: any) => void
+export type MiddlewareFunction = (req: any, res: any, event: any, context: Context, done: any) => void
